0aefcedbee9adbd45f4a3aed296089f8ac96be0e
HelAsia <asiaheluszka1993@gmail.com>
2018-11-17 13:34:21 +0100

    Initial commit

---
+++ app/src/main/java/com/helasia/stackoverflowsearcher/search_with_results/SearchActivityView.java
@@ -0,0 +1,14 @@
+package com.helasia.stackoverflowsearcher.search_with_results;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.helasia.stackoverflowsearcher.R;
+
+public class SearchActivityView extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_search_view);
+  }
+}
